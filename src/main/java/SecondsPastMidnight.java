454ee6a6498ab51d76e84e041ff34cc7e4ab2142
Vincent Ayala <vincent.227229@nv.ccsd.net>
2020-09-04 17:01:28 +0000

    I did the seconds past midnight and timestaps lab

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-
-class SecondsPastMidnight {
-    public static void main(String[] args) {
-
+class SecondsPastMidnight{
+    public static void main(String[] args){
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-
         //Reads int from user
-        int number = scanner.nextInt();
-
+        int mySecond = scanner.nextInt();
+        int myHour = 0;
+        int myMinute = 0;
         //Your code goes here
-
-
+        myHour = mySecond / 3600;
+        myMinute = mySecond / 60;
+        System.out.println("The amount of hours passed: " + myHour);
+        System.out.println("The amount of minutes passed: " + myMinute);
+        System.out.println("\nThe Time is: " + myHour + ":" + myMinute);
         // closing the scanner object
         scanner.close();
     }
-}
+} 
--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-
-class Timestamps {
-    public static void main(String[] args) {
-
+class Timestamps{
+    public static void main(String[] args){
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-
         //Reads ints from user
         int hours1 = scanner.nextInt();
         int minutes1 = scanner.nextInt();
@@ -13,13 +10,20 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
-
-
+        //Variables
+        int myFHour;
+        int myFMinute;
+        int myFSecond;
+        int myFinalAnswer;
         /*
          *  your code goes here
          */
-
-
+        myFHour = hours2 - hours1;
+        myFMinute = minutes2 - minutes1;
+        myFSecond = seconds2 - seconds1;
+        myFinalAnswer = (myFHour * 3600) + (myFMinute * 60) + myFSecond;
+        //Print Statement
+        System.out.println("The amount of seconds passed: " + myFinalAnswer);
         // closing the scanner object
         scanner.close();
     }
