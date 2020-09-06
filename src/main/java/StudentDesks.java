60b9c95cda453faa487944a8840606464afa4581
Vincent Ayala <vincent.227229@nv.ccsd.net>
2020-09-06 23:37:08 +0000

    I finished #3

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -4,14 +4,13 @@ class StudentDesks{
        //Creates a new scanner for user input
        Scanner in = new Scanner(System.in);      
        //Reads ints from user
-       int myA = (in.nextInt() / 2);
-       int myB = (in.nextInt() / 2);
-       int myC = (in.nextInt() / 2);
+       int myA = in.nextInt();
+       int myB = in.nextInt();
+       int myC = in.nextInt();
        /*
         *  your code goes here
         */
-       int myRemainder = (myA + myB + myC) % 2;
-       int myFinalAnswer = myA + myB + myC + myRemainder;
+       int myFinalAnswer = ((myA / 2) + (myA % 2)) + ((myB / 2) + (myB % 2)) + ((myC / 2) + (myC % 2));
        System.out.print(myFinalAnswer);
        // closing the scanner object
        in.close();
